ba647c6b137369f810f5aba63244e808a1adbb56
“Jessica <jmychalm13@gmail.com>
2023-08-24 20:34:23 -0500

    added article cards

--- src/ArticlesIndex.jsx
+++ src/ArticlesIndex.jsx
@@ -1,15 +1,18 @@
 export function ArticlesIndex(props) {
   return (
     <div>
-      <h1>The News!!!!</h1>
-      {props.articles.map((article) => (
-        <div key={article.title}>
-          <h3>{article.title}</h3>
-          <p>Author: {article.author}</p>
-          <p>Description: {article.description}</p>
-          <p>Content: {article.content}</p>
-        </div>
-      ))}
+      <h2>All Articles</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
+        {props.articles.map((article) => (
+          <div className="rounded mb-4 shadow-md border-2 p-4" key={article.title}>
+            <img className="aspect-[4/3] object-cover" src={article.urlToImage} />
+            <h3 className="font-bold text-xl mb-3">{article.title}</h3>
+            <p className="mb-3 italic">Author: {article.author}</p>
+            {/* <p>Description: {article.description}</p> */}
+            <div className="text-gray-700 text-base" dangerouslySetInnerHTML={{ __html: article.description }}></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
